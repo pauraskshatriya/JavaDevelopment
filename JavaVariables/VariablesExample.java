093dd40ff0d7a1711b913f0f33d97a8f74e33f4c
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-11 23:19:14 +0530

    Learned java variables

---
+++ JavaVariables/VariablesExample.java
@@ -0,0 +1,26 @@
+public class VariablesExample {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        // Double (decimal number)
+        double height = 5.9;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean (true/false)
+        boolean isStudent = true;
+
+        // String (text)
+        String name = "John";
+
+        // Printing the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
