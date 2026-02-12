cbe8d762524715641b30028953b7a7152ac97b5b
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-12 23:00:01 +0530

    Create LiteralsExample.java

---
+++ LiteralsExample/LiteralsExample.java
@@ -0,0 +1,17 @@
+package LiteralsExample;
+public class LiteralsExample {
+    public static void main(String[] args) {
+        
+        int age = 21;
+        float height = 5.9f;
+        char grade = 'A';
+        String name = "Alex";
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
