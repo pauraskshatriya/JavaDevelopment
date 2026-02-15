cbf60e805db93e00e0f2e4f7916f1961111f0bab
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-15 21:29:33 +0530

    User Input in Java

---
+++ UserInput/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Enter your name: ");
+       String name = scanner.nextLine();
+
+       System.out.println("Enter your age: ");
+       int age = scanner.nextInt();
+
+       System.out.println("What is your gpa: ");
+       double gpa = scanner.nextDouble();
+
+       System.out.println("Are you a student? (true/false): ");
+       boolean isStudent = scanner.nextBoolean();
+
+       System.out.println("Hello " + name);
+       System.out.println("You are " + age + " years old");
+       System.out.println("Your gpa is: " + gpa);
+
+       if(isStudent){
+           System.out.println("You are enrolled in classes");
+       }
+       else{
+           System.out.println("You are NOT enrolled in classes");
+       }
+
+       scanner.close(); 
+   }
+}
