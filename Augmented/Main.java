8be3faba2b9193e62a731b8c54ed8d77d1fb1516
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-17 10:35:52 +0530

    Learned how Augmented Assignment Operators work.

---
+++ Augmented/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Augmented Assignment Operators in java:");
+
+        int x = 10;
+        int y = 12;
+
+        x += y;
+
+        System.out.println(x);
+    }
+
+    
+}
