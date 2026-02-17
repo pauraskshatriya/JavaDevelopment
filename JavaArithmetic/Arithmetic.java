1bab4b0188d655fdfdbc256738911e9b15779cac
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-17 10:21:44 +0530

    Java arithmetic

---
+++ JavaArithmetic/Arithmetic.java
@@ -0,0 +1,57 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        System.out.println("These are java arithmetic operations: ");
+        System.out.println("Where x = 10 & y = 5:");
+
+        int x = 10;
+        int y = 5;
+        int z;
+
+        z = x + y;
+
+        System.out.println("z = x + y =>" +z);
+
+        System.out.println("Where a = 15 & b = 12:");
+
+        int a = 15;
+        int b = 12;
+        int c;
+
+        c = a - b;
+
+        System.out.println("c = a - b =>" + c);
+
+        System.out.println("Where d = 1.5 & e = 3.9:");
+
+        double d = 1.5;
+        double e = 3.9;
+        double f;
+
+        f = d * e;
+
+        System.out.println("f = 1.5 * 3.9 =>" + f);
+
+        System.out.println("Where g = 1.7 & h = 0.9:");
+
+        double g = 1.7;
+        double h = 0.9;
+        double i;
+
+        i = g / h;
+
+        System.out.println("g = 1.7 * 0.9 =>" + i);
+
+        
+        System.out.println("Where w = 10 & v = 3:");
+
+        double w = 10;
+        double v = 3;
+        double u;
+
+        u = w % v;
+
+        System.out.println("u = 10 % 3 =>" + u);
+    }
+    
+}
