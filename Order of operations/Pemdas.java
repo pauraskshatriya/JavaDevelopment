2ed2aa6a8326de858a80422d264ac5c9240b56eb
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-18 22:59:27 +0530

    PEMDAS
    Order of operation in java

---
+++ Order of operations/Pemdas.java
@@ -0,0 +1,8 @@
+public class Pemdas {
+    public static void main(String[] args) {
+        System.out.println("Order of Operations occurs in java using PEMDAS:");
+        double result = 3 + 4 * (7-5) / 2.0;
+        System.out.println("Result of the following operation 3 + 4 * (7-5) / 2.0 is " + result);
+    }
+    
+}
