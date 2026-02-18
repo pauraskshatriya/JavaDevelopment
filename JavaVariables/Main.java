192a63a84c50e040d24837d7166b8e617cb92742
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-02-18 22:43:31 +0530

    Updated some programs

---
+++ JavaVariables/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "XYZ";
+        int gta = 6;
+        double pi = 3.1459;
+        char gender ='M';
+        boolean isAdmin = true;
+
+        System.out.println("Your name is " + name );
+        System.out.println("Which will be the latest installment in gta series? " + gta );
+        System.out.println("Value of pi is " + pi);
+        System.out.println("What is your gender? " + gender);
+        System.out.println(isAdmin);
+    }
+}
