09fdf4871341d5164d11acc58198b9280a8a2d10
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-03-02 14:37:01 +0530

    Learned about built in constraints

---
+++ JavaMath2/def.java
@@ -0,0 +1,12 @@
+public class def {
+   public static void main(String[] args) {
+
+       System.out.println(Math.PI);
+       System.out.println(Math.E);
+
+       double result;
+
+       result = Math.pow(3, 4);
+       System.out.println(result);
+   }
+}
