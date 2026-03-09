8ce9bc73e3cc5aa5152918c02776a70a099e2eae
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-03-09 23:50:52 +0530

    Update Flip.java

--- HeadsorTails/Flip.java
+++ HeadsorTails/Flip.java
@@ -3,15 +3,8 @@ import java.util.Random;
 public class Flip {
     public static void main(String[] args) {
         Random random = new Random();
-        boolean isHeads;
+        boolean isHeads = random.nextBoolean();
 
-        isHeads = random.nextBoolean();
-
-        if(isHeads){
-            System.out.println("HEADS");
-        }
-        else{
-            System.out.println("TAILS");
-        }
+        System.out.println(isHeads ? "HEADS" : "TAILS");
     }
 }
