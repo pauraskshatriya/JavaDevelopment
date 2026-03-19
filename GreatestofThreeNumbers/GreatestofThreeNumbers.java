236193c1cc3810f354803d0fe739a338e60c72c9
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-03-19 23:25:35 +0530

    Update GreatestofThreeNumbers.java

--- GreatestofThreeNumbers/GreatestofThreeNumbers.java
+++ GreatestofThreeNumbers/GreatestofThreeNumbers.java
@@ -1,28 +1,37 @@
-import java.util.Scanner;
+import java.util.Scanner; // Fixed capitalization of 'import'
 
-class GreatestofThreeNumbers {
+class GreatestOfThreeNumbers {
 
-    void findLargest(int a, int b, int c) {
+    // Made static so we don't need to create an object in main
+    static void findLargest(int a, int b, int c) {
         if (a >= b && a >= c) {
-            System.out.println("First number is greatest");
+            System.out.println("First number (" + a + ") is the greatest.");
         } 
-        else if (b >= a && b >= c) {
-            System.out.println("Second number is greatest");
+        // Simplified logic: If we reach here, 'a' is definitely not the greatest.
+        // Therefore, we only need to compare 'b' and 'c'.
+        else if (b >= c) {
+            System.out.println("Second number (" + b + ") is the greatest.");
         } 
         else {
-            System.out.println("Third number is greatest");
+            System.out.println("Third number (" + c + ") is the greatest.");
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Added prompts so the user knows what to do
+        System.out.print("Enter the first number: ");
         int a = sc.nextInt();
+        
+        System.out.print("Enter the second number: ");
         int b = sc.nextInt();
+        
+        System.out.print("Enter the third number: ");
         int c = sc.nextInt();
 
-        GreatestofThreeNumbers obj = new GreatestofThreeNumbers();
-        obj.findLargest(a, b, c);
+        // Called directly without instantiation because the method is now static
+        findLargest(a, b, c);
 
         sc.close();
     }
