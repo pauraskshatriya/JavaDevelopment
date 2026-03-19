72d52d51d99eac6d42de8bbe40356adf123e57ca
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-03-19 23:26:42 +0530

    Update Main.java

--- Increment&DecrementOperators/Main.java
+++ Increment&DecrementOperators/Main.java
@@ -1,22 +1,25 @@
-public class Main {
+public class Main { // Fixed capitalization of 'public'
+
     public static void main(String[] args) {
-        System.out.println("Here I am using Java's Increment & Decrement Operators:\n"
-                + "Using x = 1 & y = 3\n"
-                + "First, we will increment x.\n"
-                + "Then, we will decrement y.\n"
-                + "Using x++ twice and y-- twice");
+        // Splitting the large print statement makes the code easier to read
+        System.out.println("Here I am using Java's Increment & Decrement Operators:");
+        System.out.println("Initial values: x = 1 & y = 3");
+        System.out.println("First, we will increment x twice.");
+        System.out.println("Then, we will decrement y twice.\n");
 
-                int x = 1;
-                int y = 3;
+        int x = 1;
+        int y = 3;
 
-                x++;
-                x++;
+        // Increment x twice (x++ is shorthand for x = x + 1)
+        x++;
+        x++;
 
-                y--;
-                y--;
+        // Decrement y twice (y-- is shorthand for y = y - 1)
+        y--;
+        y--;
 
-                System.out.println("Value of x after using Increment operator: " + x);
-                System.out.println("Value of y after using Decrement operator: " + y);
-
+        // Using printf (print formatted) for cleaner output without using the '+' sign
+        System.out.printf("Value of x after using Increment operator: %d%n", x);
+        System.out.printf("Value of y after using Decrement operator: %d%n", y);
     }
 }
