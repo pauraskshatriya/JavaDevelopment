a6ff8fc1607b07b13ea3c6a5d759270ca77e88a2
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-04-07 23:08:23 +0530

    Update Demo.java

--- AreaofCircle/Demo.java
+++ AreaofCircle/Demo.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of circle: ");
         
-        System.out.println("Enter radius of circle: ");
-        double r = sc.nextDouble();
-        
-        double area = Math.PI * r * r;
-        
-        System.out.println("Area of circle: " + area);
-        
+        if (sc.hasNextDouble()) {
+            double r = sc.nextDouble();
+
+            if (r >= 0) {
+                double area = Math.PI * r * r;
+                System.out.printf("Area of circle: %.2f%n", area);
+            } else {
+                System.out.println("Radius cannot be negative.");
+            }
+        } else {
+            System.out.println("Invalid input. Please enter a number.");
+        }
+
         sc.close();
     }
 }
