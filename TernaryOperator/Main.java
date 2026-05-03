e88f2319a25dc9b377449752daaa06e1e85fd995
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-05-03 23:34:11 +0530

    Update Main.java

--- TernaryOperator/Main.java
+++ TernaryOperator/Main.java
@@ -1,9 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
         int number = 10;
 
-        String result = (number % 2 == 0) ? "Even" : "Odd";
+        String parity = (number % 2 == 0) ? "Even" : "Odd";
 
-        System.out.println("The number is: " + result);
+        System.out.println("The number " + number + " is " + parity + ".");
     }
 }
