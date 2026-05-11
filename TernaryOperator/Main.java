739824062d2af97d58124166c47e478a9ce39323
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-05-11 23:11:44 +0530

    Update Main.java

--- TernaryOperator/Main.java
+++ TernaryOperator/Main.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 10;
 
-        String parity = (number % 2 == 0) ? "Even" : "Odd";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("The number " + number + " is " + parity + ".");
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println("The number " + number + " is " + result + ".");
+
+        scanner.close();
     }
 }
