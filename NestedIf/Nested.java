9d95142b97d1781d2900f4f75e9a9a5691894d17
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-05-13 18:01:51 +0530

    Update Nested.java

--- NestedIf/Nested.java
+++ NestedIf/Nested.java
@@ -5,23 +5,22 @@ public class Nested {
         boolean isSenior = false;
         double price = 9.99;
 
-        if (isStudent) {
-            if (isSenior) {
-                System.out.println("You are a senior discount of 20%");
-                System.out.println("You are a student discount of 10%");
-                price *= 0.7;
-            } else {
-                System.out.println("You get a student discount of 10%");
-                price *= 0.9;
-            }
+        double discount = 1.0;
+
+        if (isStudent && isSenior) {
+            System.out.println("You get both student (10%) and senior (20%) discount");
+            discount = 0.7;
+        } else if (isStudent) {
+            System.out.println("You get a student discount of 10%");
+            discount = 0.9;
+        } else if (isSenior) {
+            System.out.println("You get a senior discount of 20%");
+            discount = 0.8;
         } else {
-            if (isSenior) {
-                System.out.println("You get a senior discount of 20%");
-                price *= 0.8;
-            } else {
-                price *= 1;
-            }
+            System.out.println("No discount applied");
         }
+
+        price *= discount;
 
         System.out.printf("The price of a ticket is: $%.2f", price);
     }
