79d242655fdb03149e0241a3fe76d55f6f96e20f
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-05-23 07:49:17 +0530

    Update Nested.java

--- NestedIf/Nested.java
+++ NestedIf/Nested.java
@@ -1,27 +1,35 @@
 public class Nested {
     public static void main(String[] args) {
-
         final boolean isStudent = true;
         final boolean isSenior = false;
-        double price = 9.99;
 
-        double discount = 1.0;
+        // Constants for pricing and discounts
+        final double BASE_PRICE = 9.99;
+        final double STUDENT_DISCOUNT_RATE = 0.10;
+        final double SENIOR_DISCOUNT_RATE = 0.20;
+
+        // Combined discount: 10% + 20% = 30% off → multiplier = 0.7
+        final double COMBINED_DISCOUNT_MULTIPLIER = 1.0 - (STUDENT_DISCOUNT_RATE + SENIOR_DISCOUNT_RATE);
+        final double STUDENT_ONLY_MULTIPLIER = 1.0 - STUDENT_DISCOUNT_RATE;
+        final double SENIOR_ONLY_MULTIPLIER = 1.0 - SENIOR_DISCOUNT_RATE;
+
+        double discountMultiplier = 1.0; // No discount by default
 
         if (isStudent && isSenior) {
             System.out.println("You get both student (10%) and senior (20%) discount");
-            discount = 0.7;
+            discountMultiplier = COMBINED_DISCOUNT_MULTIPLIER;
         } else if (isStudent) {
             System.out.println("You get a student discount of 10%");
-            discount = 0.9;
+            discountMultiplier = STUDENT_ONLY_MULTIPLIER;
         } else if (isSenior) {
             System.out.println("You get a senior discount of 20%");
-            discount = 0.8;
+            discountMultiplier = SENIOR_ONLY_MULTIPLIER;
         } else {
             System.out.println("No discount applied");
         }
 
-        double finalPrice = price * discount;
+        double finalPrice = BASE_PRICE * discountMultiplier;
 
         System.out.printf("The price of a ticket is: $%.2f%n", finalPrice);
     }
-}
+}   
