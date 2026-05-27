f51bebbea9c7fe65ca66a05975ff93db39add12d
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-05-27 08:47:00 +0530

    Update Flip.java

--- HeadsorTails/Flip.java
+++ HeadsorTails/Flip.java
@@ -2,24 +2,11 @@ import java.util.Random;
 
 public class Flip {
     public static void main(String[] args) {
+        final Random random = new Random();
+        final boolean isHeads = random.nextBoolean(); // cleaner than nextInt(2)
+        final String result = isHeads ? "HEADS" : "TAILS";
 
-        // Create Random object
-        Random random = new Random();
-
-        // Generate 0 or 1
-        int toss = random.nextInt(2);
-
-        // Decide result
-        String result = (toss == 0) ? "HEADS" : "TAILS";
-
-        // Display result
-        System.out.println("🪙 Coin Toss Result: " + result);
-
-        // Extra feedback
-        if (result.equals("HEADS")) {
-            System.out.println("You got lucky with Heads!");
-        } else {
-            System.out.println("It's Tails this time!");
-        }
+        System.out.println("Coin Toss Result: " + result);
+        System.out.println(isHeads ? "You got lucky with Heads!" : "It's Tails this time!");
     }
 }
