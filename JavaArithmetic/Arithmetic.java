7fa2df5543bea2252dd22297bae7287b9dc7b037
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-05-31 09:39:55 +0530

    Update Arithmetic.java

--- JavaArithmetic/Arithmetic.java
+++ JavaArithmetic/Arithmetic.java
@@ -1,57 +1,22 @@
 public class Arithmetic {
     public static void main(String[] args) {
+        System.out.println("Java arithmetic operations:");
 
-        System.out.println("These are java arithmetic operations: ");
-        System.out.println("Where x = 10 & y = 5:");
+        // Integer ops
+        int x = 10, y = 5;
+        System.out.printf("x + y => %d + %d = %d%n", x, y, x + y);
 
-        int x = 10;
-        int y = 5;
-        int z;
+        int a = 15, b = 12;
+        System.out.printf("a - b => %d - %d = %d%n", a, b, a - b);
 
-        z = x + y;
+        // Double ops
+        double d = 1.5, e = 3.9;
+        System.out.printf("d * e => %.1f * %.1f = %.2f%n", d, e, d * e);
 
-        System.out.println("z = x + y =>" +z);
+        double g = 1.7, h = 0.9;
+        System.out.printf("g / h => %.1f / %.1f = %.3f%n", g, h, g / h);
 
-        System.out.println("Where a = 15 & b = 12:");
-
-        int a = 15;
-        int b = 12;
-        int c;
-
-        c = a - b;
-
-        System.out.println("c = a - b =>" + c);
-
-        System.out.println("Where d = 1.5 & e = 3.9:");
-
-        double d = 1.5;
-        double e = 3.9;
-        double f;
-
-        f = d * e;
-
-        System.out.println("f = 1.5 * 3.9 =>" + f);
-
-        System.out.println("Where g = 1.7 & h = 0.9:");
-
-        double g = 1.7;
-        double h = 0.9;
-        double i;
-
-        i = g / h;
-
-        System.out.println("g = 1.7 * 0.9 =>" + i);
-
-        
-        System.out.println("Where w = 10 & v = 3:");
-
-        double w = 10;
-        double v = 3;
-        double u;
-
-        u = w % v;
-
-        System.out.println("u = 10 % 3 =>" + u);
+        double w = 10, v = 3;
+        System.out.printf("w %% v => %.0f %% %.0f = %.0f%n", w, v, w % v);
     }
-    
 }
